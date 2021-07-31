a418dd12d9906d29914c8ac3ad9a8bb179ac576c
Jagadeesh Thottempudi <jagadeeshthottempudi@gmail.com>
2021-07-31 20:58:08 +0530

    Initialized javaScript

---
+++ tip-calculator-app-main/tip-calculator-app-main/index.js
@@ -0,0 +1,6 @@
+var bill = parseInt(document.querySelector(".bill-input").value);
+// function myFunction(btn)
+// {
+//     console.log(document.querySelector("#"+btn.id).value);
+// }
+console.log(bill);
