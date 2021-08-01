b5b54cecb4ec643ad5991be5c25b48529aa50a22
Jagadeesh Thottempudi <jagadeeshthottempudi@gmail.com>
2021-08-01 13:56:39 +0530

    Complted JavaScript for this challenge

--- tip-calculator-app-main/tip-calculator-app-main/index.js
+++ tip-calculator-app-main/tip-calculator-app-main/index.js
@@ -1,6 +1,50 @@
-var bill = parseInt(document.querySelector(".bill-input").value);
-// function myFunction(btn)
-// {
-//     console.log(document.querySelector("#"+btn.id).value);
-// }
-console.log(bill);
+var flag=0;
+document.querySelector("#input1").addEventListener("keyup",
+    function (event) {
+        var bill = event.target.value;
+    });
+for (var i = 1; i <= 5; i++) {
+    document.querySelector("#button-" + i).addEventListener("click",
+        function (event) {
+            event.target.classList.add("button-selected");
+            // setTimeout(function(){event.target.classList.remove("button-selected")},1000);
+        });
+}
+document.querySelector("#input2").addEventListener("keyup",
+    function (event) {
+        var custom_percent = event.target.value;
+        // percent(custom_percent);
+    });
+document.querySelector("#input3").addEventListener("keyup",
+    function (event) {
+        var people = event.target.value;
+        // division(custom_percent);
+        extract();
+    });
+
+function extract() {
+    var bill = document.querySelector("#input1").value;
+    var people = document.querySelector("#input3").value;
+    var percent;
+    for (var i = 1; i <= 5; i++) {
+        var boole = document.querySelector("#button-"+i).classList.contains("button-selected");
+        if (boole == true) {
+            percent = document.querySelector(".button-selected").value;
+            flag=1;
+        }
+    }
+    if (flag==0) {
+        percent= document.querySelector("#input2").value;
+    }
+    if (people==0) {
+        document.querySelector(".warning").style.visibility = "visible";
+    }
+    else
+    {
+        document.querySelector(".warning").style.visibility="hidden";
+    }
+    var tip = Number((((percent/100)*bill)/people).toFixed(2));
+    document.querySelector(".tip-amount").innerHTML = "$"+tip;
+    var total = Number(tip)+Number(bill/people);
+    document.querySelector(".total-amount").innerHTML = "$"+total.toFixed(2);
+}
